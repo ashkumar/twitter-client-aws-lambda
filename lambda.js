499e04e5113db2cbcae19a72f0a1c1594981a62c
Andriyan Ivanov <andriyan.ivanov@gmail.com>
2018-03-12 21:51:18 +0200

    CLEANUP lambda & helpers

--- lambda.js
+++ lambda.js
@@ -1,55 +1,15 @@
-const { TwitterClient } = require('./lib/TwitterClient');
 const { Response } = require('./lib/Response');
-const helpers = require('./lib/helpers');
-const config = require('./config');
+const { Controller } = require('./lib/Controller');
 
 exports.execute = (event, context, callback) => {
-  const client = new TwitterClient(config.twitter);
-  const param = helpers.getParam.bind(null, event);
+  const controller = new Controller(event);
 
-  let query;
-  const action = param('action');
-
-  switch(action) {
-    case 'search':
-      query = client.search({
-        q: param('q'),
-        max_id: param('max_id'),
-        count: param('count'),
-        lang: param('lang'),
-        geocode: param('geocode'),
-        locale: param('locale')
-      });
-      break;
-
-    case 'user_search':
-      query = client.userSearch({
-        q: param('q'),
-        page: param('page'),
-        count: param('count'),
-        include_entities: false
-      });
-      break;
-
-    case 'posts_by_user':
-      query = client.getPostsByScreenName(param('user'));
-      break;
-    
-    case 'posts_by_user_id':
-      query = client.getPostsByUserId(param('id'));
-      break;
-
-    case 'tweet':
-      query = client.getTweet(param('id'));
-      break;
-    
-    default:
-      const error = `invalid API action called: ${action}`;
-      const response = JSON.stringify({ error });
-
-      return callback(undefined, Response.badRequest(response));
+  if(!controller.action) {
+    const error = `invalid API action called: ${controller.actionName}.`;
+    return callback(undefined, Response.badRequest({ error }));
   }
 
-  return query.then(data => callback(undefined, Response.OK(data)))
+  return controller.action()
+    .then(data => callback(undefined, Response.OK(data)))
     .catch(error => callback(undefined, Response.serverError(error)));
 }
