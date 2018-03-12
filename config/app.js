30578d2f04051f8eab9a5c5df7741dad3ad5ec53
Andriyan Ivanov <andriyan.ivanov@gmail.com>
2018-03-12 18:11:10 +0200

    EXTENDED TwitterClient

--- config/app.js
+++
@@ -1,7 +0,0 @@
-exports.twitter =	{
-  'consumerKey':       'YOUR_CONSUMER_KEY',
-  'consumerSecret':    'YOUR_CONSUMER_SECRET',
-  'accessToken':       'YOUR_ACCESS_TOKEN',
-  'accessTokenSecret': 'YOUR_ACCESS_TOKEN_SECRET',
-  'callBackUrl':       'YOUR_CALLBACK_URL',
-};
