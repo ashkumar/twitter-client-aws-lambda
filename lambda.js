d2754b115e981bee90746552082db4f91ea28bc8
Andriyan Ivanov <andriyan.ivanov@gmail.com>
2018-03-14 18:51:14 +0200

    CHANGED executing lambda in try/catch

--- lambda.js
+++ lambda.js
@@ -2,14 +2,18 @@ const { Response } = require('./lib/Response');
 const { Controller } = require('./lib/Controller');
 
 exports.execute = (event, context, callback) => {
-  const controller = new Controller(event);
+  try {
+    const controller = new Controller(event);
 
-  if(!controller.action) {
-    const error = `invalid API action called: ${controller.actionName}.`;
-    return callback(undefined, Response.badRequest({ error }));
+    if(!controller.action) {
+      const error = `invalid API action called: ${controller.actionName}.`;
+      return callback(undefined, Response.badRequest({ error }));
+    }
+  
+    return controller.action()
+      .then(data => callback(undefined, Response.OK(data)))
+      .catch(error => callback(undefined, Response.serverError(error)));
+  } catch(e) {
+    return callback(undefined, Response.serverError(e));
   }
-
-  return controller.action()
-    .then(data => callback(undefined, Response.OK(data)))
-    .catch(error => callback(undefined, Response.serverError(error)));
 }
